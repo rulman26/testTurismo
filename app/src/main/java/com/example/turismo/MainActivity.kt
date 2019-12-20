d15a0d188a1153abe246502f0c4868d8e7eb0f46
rulman <rulman26@gmail.com>
2019-12-19 23:06:09 -0500

    mi primer commit

--- app/src/main/java/com/example/turismo/MainActivity.kt
+++ app/src/main/java/com/example/turismo/MainActivity.kt
@@ -8,6 +8,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var xx="rulman"
+        val nombre: String
+        nombre="Rulman"
+        println(nombre)
     }
 }
